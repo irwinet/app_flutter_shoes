a135a3c5358e0227cb427276542cd47f4cae854f
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-09-08 18:35:10 -0500

    Add Description

--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,3 +1,4 @@
 
+export 'package:app_flutter_shoes/src/widgets/zapato_desc.dart';
 export 'package:app_flutter_shoes/src/widgets/zapato_size.dart';
 export 'package:app_flutter_shoes/src/widgets/custom_appbar.dart';
