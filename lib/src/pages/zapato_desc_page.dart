f995aefe5efb6ef3e7d0b8f69d7812a149d51bdb
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-09-09 22:54:07 -0500

    Add Monto - Button Buy

--- lib/src/pages/zapato_desc_page.dart
+++ lib/src/pages/zapato_desc_page.dart
@@ -19,11 +19,36 @@ class ZapatoDescPage extends StatelessWidget {
                       titulo: 'Nike Air Max 720',
                       descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                     ),
+
+                  _MontoBuyNow()
                 ],
               ),
             )
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _MontoBuyNow extends StatelessWidget {
+  const _MontoBuyNow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Row(
+          children: [
+            Text('\$180.0', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
+            Spacer(),
+            BotonNaranja(texto: 'Buy now', ancho: 120, alto: 40,)
+          ],
+        ),
       ),
     );
   }
